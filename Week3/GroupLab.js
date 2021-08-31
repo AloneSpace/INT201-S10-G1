a682a6e1d9c31be8aa37bb062fcd8d58d16660e0
AloneSpace <43822593+AloneSpace@users.noreply.github.com>
2021-08-31 09:50:06 +0700

    Update GroupLab.js

--- Week3/GroupLab.js
+++ Week3/GroupLab.js
@@ -1,64 +1,9 @@
-//* Declare employess
-let employees = [
-    {
-        name: "Karunpat Promvisut",
-        id: 63130500002,
-        yearActive: 5,
-        salary: 40000,
-    },
-    {
-        name: "Chomkanok Chuenpapai",
-        id: 63130500016,
-        yearActive: 5,
-        salary: 25000,
-    },
-    {
-        name: "Tripuwarej Paweenasombat",
-        id: 63130500040,
-        yearActive: 3,
-        salary: 30000,
-    },
-    {
-        name: "Trongsit Sinnurak",
-        id: 63130500043,
-        yearActive: 4,
-        salary: 28000,
-    },
-    {
-        name: "Thatphum Kongnithichalerm",
-        id: 63130500044,
-        yearActive: 2,
-        salary: 38000,
-    },
-    {
-        name: "Naruebadin Ameenee",
-        id: 63130500067,
-        yearActive: 1,
-        salary: 35000,
-    },
-];
-
-const yearAcceptBonus = 3;
-
-//* Declare function getBonus to calculate salary + bonus
-function getBonusWithSalary(employee) {
-    if (employee.yearActive >= yearAcceptBonus)
-        return employee.salary + (employee.salary * yearAcceptBonus) / 100;
-    return employee.salary;
-
-    //*Ternary condition
-    // return yearAcceptBonus >= 3 ? employee.salary + (employee.salary * yearAcceptBonus) / 100; : employee.salary
+const getBMI = function (pound = 0, inch = 0) {
+  let kilogram = +pound * 0.453592
+  let weight = +inch * 0.0254
+  return kilogram / Math.pow(weight, 2)
 }
 
-//* Declare function getSalaryText to format text
-function getSalaryText(employee, fn) {
-    return `${employee.name} - ${fn(employee)} บาท`;
-}
-
-console.log(`Get length of employees ${employees.length} person\n`);
-console.log("Get all employee bonus (Wait for 1 Sec)\n");
-employees.forEach((employee) =>
-    setTimeout(() => {
-        console.log(getSalaryText(employee, getBonusWithSalary));
-    }, 1000)
-);
+console.log(getBMI(36.5, 39)) //16.871807182141648
+console.log(getBMI(100.0, 55)) //23.241950616132637
+console.log(getBMI(120.0, 66)) //23.241950616132637
