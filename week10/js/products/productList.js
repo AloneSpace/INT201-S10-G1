169e896619d3fa53bf55ce699fa5a67443826f26
Thatphum <68836892+Thatphum@users.noreply.github.com>
2021-11-02 17:50:46 +0700

    Update productList.js

--- week10/js/products/productList.js
+++ week10/js/products/productList.js
@@ -1,72 +1,26 @@
 import stationeries from "../stationeries.js";
 
 const productsEle = document.querySelector("#products");
-const incartEle = document.querySelector("#incart");
-const carts = document.querySelector("#carts");
-
-let count = 0;
-incartEle.textContent = count;
-let incart = [];
-
-let result = stationeries.filter(text => text.name.includes(""));
-
-let btn = document.getElementById("btn");
-
-btn.addEventListener("click" , ()=>{
-    buttonC();
-},false)
-
-//ปุ่ม search
-function buttonC(){
-    let x = document.getElementById("search").value.toLowerCase();
-    result = stationeries.filter(text => text.name.toLowerCase().includes(`${x}`));
-    console.log(result.length == 0);
-    let divProduct = productsEle.children;
-    if(result.length == 0){
-        for(let a of divProduct){
-            a.style.display = "none";
-        }
-    }else{
-        for(let a of divProduct){
-            for(let e of result){
-                if(a.id == e.id){
-                    a.style.display = "";
-                    break;
-                }else{
-                    a.style.display = "none";
-                }
-            }
-        }
-    }
-}
-//เลขตะกร้า
-carts.addEventListener("click" , ()=>{
-    let a = [];
-    incart = a;
-    incartEle.textContent = count = 0;
-    console.log(incart);
-})
 
 for (let stationery of stationeries) {
-        let divProductEle = document.createElement("div");
-        divProductEle.setAttribute("id", stationery.id);
-        divProductEle.className =
-            "w-56 md:w-72 bg-white mt-20 shadow-lg rounded-lg overflow-hidden";
-        let divProductName = appendProductName(stationery);
-        let divImgProduct = appendImgProduct(stationery);
-        let divProductDetail = appendProductDetail(stationery);
-    
-        divProductEle.appendChild(divProductName);
-        divProductEle.appendChild(divImgProduct);
-        divProductEle.appendChild(divProductDetail);
-        productsEle.appendChild(divProductEle);
+    let divProductEle = document.createElement("div");
+    divProductEle.setAttribute("id", stationery.id);
+    divProductEle.className =
+        "w-56 md:w-72 bg-white mt-20 shadow-lg rounded-lg overflow-hidden";
+    let divProductName = appendProductName(stationery);
+    let divImgProduct = appendImgProduct(stationery);
+    let divProductDetail = appendProductDetail(stationery);
+
+    divProductEle.appendChild(divProductName);
+    divProductEle.appendChild(divImgProduct);
+    divProductEle.appendChild(divProductDetail);
+    productsEle.appendChild(divProductEle);
 }
 
 function appendProductName(stationery) {
     let divProductName = document.createElement("div");
     divProductName.className =
         "py-2 text-center font-bold uppercase tracking-wide text-gray-800";
-    divProductName.setAttribute("id" , "name");
     divProductName.textContent = stationery.name;
     return divProductName;
 }
@@ -78,15 +32,6 @@ function appendProductDetail(stationery) {
     let divPriceEle = appendDivPriceEle(stationery);
     let addToCartBtn = document.createElement("button");
     addToCartBtn.setAttribute("id", stationery.name);
-    addToCartBtn.addEventListener("click", ()=>{
-        let addproduct = addToCartBtn.getAttribute('id')
-        // if(!checks(incart , addproduct)){
-        //     incartEle.textContent =  ++count;
-        // };
-        incartEle.textContent =  ++count;
-        incart.push(addproduct);
-        console.log(incart);
-    },false);
     addToCartBtn.className =
         "bg-green-500 text-xs text-white px-2 py-1 font-semibold rounded uppercase hover:bg-green-600";
     addToCartBtn.textContent = "เพิ่มลงในตะกร้า";
@@ -119,11 +64,3 @@ function appendImgProduct(stationery){
     divImgProduct.appendChild(ImgEle);
     return divImgProduct;
 }
-
-
-function checks(arr, val) {
-    return arr.some(function(arrVal) {
-      return val === arrVal;
-    });
-  }
-
